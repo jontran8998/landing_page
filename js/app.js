c696b6be24e84fccd7c3a2724ada9b08f951e70d
Jon Tran <trungt8998@gmail.com>
2021-08-06 23:14:26 -0500

    Build Nav

--- js/app.js
+++ js/app.js
@@ -18,13 +18,18 @@
  * 
 */
 
+// Get navbarList by ID
+const navbarList = document.getElementById("navbar__list");
+
+// Get an array of multiple sections
+const sections = document.querySelectorAll("section");
+
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
 
 
 /**
@@ -34,6 +39,37 @@
 */
 
 // build the nav
+const navBuild = (sections) => {
+  // Loop over the list of sections to build navbar menu items
+  for (section of sections) {
+    // Create <li> element
+    const navLi = document.createElement('li');
+
+    // Create <a> tag
+    const navLink = document.createElement('a');
+
+    // Add href, id, classList to <a> tag
+    navLink.href = `#${section.id}`;
+    navLink.id = `nav_${section.id}`;
+    navLink.classList = 'menu__link';
+
+    // Add event listener to nav links for 'click' event to scroll to the corresponding section
+    navLink.addEventListener('click', () => {
+      section.scrollIntoView({behavior: 'smooth'})
+    })
+
+    // Get nav name
+    const text = document.createTextNode(`${section.dataset.nav}`);
+
+    // Append elements
+    navLink.appendChild(text);
+    navLi.appendChild(navLink);
+    navbarList.appendChild(navLi);
+  }
+};
+
+
+
 
 
 // Add class 'active' to section when near top of viewport
@@ -50,6 +86,11 @@
 
 // Build menu 
 
+// Check if section is null or not
+if (sections.length != 0) {
+  navBuild(sections)
+}
+   
 // Scroll to section on link click
 
 // Set sections as active
